743bf6ed343eb0eccd40c2e97b73cfc379a8f926
Maria Hornero Vazquez <mhornero@c2r2s3.42madrid.com>
2022-12-06 18:59:08 +0100

    monitoring final version

--- monitoring.sh
+++ monitoring.sh
@@ -1,39 +1,65 @@
 #!/bin/bash
 
+#arquitectura
 arq=$(uname -a)
-ncpu=$(grep physical /proc/cpuinfo | sort | uniq | wc -l)
-vcpu=$(grep "processor" /proc/cpuinfo | wc -l)
+#uname--> Unix name, comando para imprimir el nombre de la máquina y otra información relevante acerca de la misma.
 
-fram=$(free -m | awk 'NR == 2{print $4"Mb"}')
-uram=$(free -m | awk 'NR == 2{print $3}')
-pram=$(free -m | awk 'NR == 2{printf("%.2f"), $3/$4*100}')
+#Número de núcleos físicos
+#/proc/cpuinfo
+ncpu=$(grep physical /proc/cpuinfo |sort | uniq | wc -l)
+#Número de núcleos virtuales
+#/proc/cpuinfo
+vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
 
+#Memoria RAM disponible y su porcentaje de uso
+#free enseña la memoria --> /proc/meminfo
+fram=$(free -m | awk 'NR == 1{print $2}')
+uram=$(free -m | awk 'NR == "Mem:" {print $3}')
+#porcentaje actual del uso de tus núcleos o ram
+pram=$(free -m |awk '/Mem/ {printf("%.3f"), $3/$2*100}')
+
+#Memoria DISCO y su porcentaje de uso
+#df --> report file system disk space usage
 fdisk=$(df -Bg | awk '/^\/dev/{fd+=$4}END{print fd"G"}')
-udisk=$(df -Bg | awk '/^\/dev/{ud+=$3}END{print ud"G"}')
-pdisk=$(df -Bg | awk '/^\/dev/{fd+=$4}{ud+=$3}END{printf("%dG"), ud/fd*100}')
+udisK=$(df -Bg | awk '/^\/dev{ud+=$2}END{print ud"G"}')
+pdisk=$(df -Bg | awk '/^\/dev{fd+=$4}{ud+=$3}END{printf("%dG"), ud/fd*100}')
 
+#Porcentaje actual del uso de mis núcleos
 cpul=$(top -bn1 | grep '^%Cpu' | cut -d: -f2 | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-ureinicio=$(who -b | awk '{print $4 " " $5}')
 
-lvms=$(lsblk | grep lvm | wc -l | tr -d '\n')
-lvme=$(if [ "$lvms" -ne "0" ]; then echo YES ; else echo NO; fi)
+#FECHA Y HORA DEL ULTIMO REINICIO
+ureinicio=$(who -b | awk '/system/{print $3" "$4}')
 
-ctcp=$(cat /proc/net/sockstat | awk '/TCP:/{print $3}')
-ulogged=$(users | wc -l)
+#LVM (logical volume manager), admin de discos para el kernel
+# 1) buscar si está en uso
+lvms=$(lbslk | grep lvm | wc -l)
+# 2) Expresar si está o no en uso
+lvme=$(if [ "$lvms" = 0 ]; then echo YES; else echo NO; fi)
 
+#Número de conexiones activas --> TCP
+#file --> /proc/net/sockstat ... coger las conexiones TCP
+ctcp=$(cat /proc/net/sockstat | awk '/TCP:{print $3}')
+
+#NUMERO DE USUARIOS CONECTADOS EN EL SERVER
+ulogged=$(users | wc -c)
+
+#IP y MACS
 dip=$(hostname -I)
-MAC=$(ifconfig | grep ether | awk '{print $2}')
-sudocdm=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+MAC=$(ip link show | awk '/link\/ether/{print $2}')
 
-wall "	#Arquitectura: $arq
+#NUMERO DE COMANDOS SUDO EJECUTADOS
+sudocmd=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+
+wall "	#Arquitectura: $arc
 	#Nucleos físicos CPU: $ncpu
 	#Nucleos lógicos CPU: $vcpu
-	#RAM: $uram/$fram	($pram%)
-	#Memoria disco: $udisk/$fdisk	($pdisk%)
-	#Uso de nucleos: $cpul
+	#RAM: $uram/$fram  ($pram%)
+	#MEMORIA DISCO: $udisk/$fdisk ($pdisk%)
+	#USO DE NUCLEOS: $cpul
 	#Último reinicio: $ureinicio
 	#Uso de LVM: $lvme
 	#Conexiones establecidas: $ctcp
-	#Usuarios contectados: $ulogged
-	#Network: IP $dip     ($MAC)
-	#CMD sudo: $sudocdm "
+	#Usuarios conectados: $ulogged
+	#Network: IP $dip ($MAC)
+	#CMD SUDO: $sudocmd"
+#WaLl es lo que hace broadcast al resto de ususarios conectados
